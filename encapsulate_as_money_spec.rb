e434e29b94fb5310dfe9c869f29eca1efb1914a8
Anthony Sellitti <anthony.sellitti@envato.com>
2014-02-25 12:23:06 +1100

    visually group classes in spec with tests

--- encapsulate_as_money_spec.rb
+++ encapsulate_as_money_spec.rb
@@ -14,9 +14,51 @@ class NilPreservingExample < Base
   encapsulate_as_money :zero_amount, :nonzero_amount, :nil_amount, :preserve_nil => true
 end
 
+describe EncapsulateAsMoney do
+  let(:nil_amount) { nil }
+  let(:zero_amount) { 0 }
+  let(:nonzero_amount) { 1_00 }
+
+  before do
+    product.instance_variable_set(:@nil_amount, nil_amount)
+    product.instance_variable_set(:@zero_amount, zero_amount)
+    product.instance_variable_set(:@nonzero_amount, nonzero_amount)
+  end
+
+  context 'dont preserve nil values' do
+    let(:product) { NilDestroyingExample.new }
+
+    it 'casts non zero integer values to a money instance' do
+      expect(product.nonzero_amount).to eq Money.new(1_00)
+    end
+
+    it 'casts zero integer values to a money instance' do
+      expect(product.zero_amount).to eq Money.new(0_00)
+    end
+
+    it 'casts nil into a zero money instance' do
+      expect(product.nil_amount).to eq Money.new(0_00)
+    end
+  end
+
+  context 'preserve nil values' do
+    let(:product) { NilPreservingExample.new }
+
+    it 'casts non zero integer values to a money instance' do
+      expect(product.nonzero_amount).to eq Money.new(1_00)
+    end
+
+    it 'casts zero integer values to a money instance' do
+      expect(product.zero_amount).to eq Money.new(0_00)
+    end
+
+    it 'preserves nil' do
+      expect(product.nil_amount).to eq nil
+    end
+  end
+end
 
 describe EncapsulateAsMoney do
-
   describe Money do
     it 'creates a Money object without using `new` (?!?)' do
       expect(Money(1_00)).to eq Money.new(1_00)
@@ -28,50 +70,6 @@ describe EncapsulateAsMoney do
       end
 
       specify { expect(Money('$10,000.00')).to eq Money.new(10000_00) }
-    end
-  end
-
-  describe EncapsulateAsMoney do
-    let(:nil_amount) { nil }
-    let(:zero_amount) { 0 }
-    let(:nonzero_amount) { 1_00 }
-
-    before do
-      product.instance_variable_set(:@nil_amount, nil_amount)
-      product.instance_variable_set(:@zero_amount, zero_amount)
-      product.instance_variable_set(:@nonzero_amount, nonzero_amount)
-    end
-
-    context 'dont preserve nil values' do
-      let(:product) { NilDestroyingExample.new }
-
-      it 'casts non zero integer values to a money instance' do
-        expect(product.nonzero_amount).to eq Money.new(1_00)
-      end
-
-      it 'casts zero integer values to a money instance' do
-        expect(product.zero_amount).to eq Money.new(0_00)
-      end
-
-      it 'casts nil into a zero money instance' do
-        expect(product.nil_amount).to eq Money.new(0_00)
-      end
-    end
-
-    context 'preserve nil values' do
-      let(:product) { NilPreservingExample.new }
-
-      it 'casts non zero integer values to a money instance' do
-        expect(product.nonzero_amount).to eq Money.new(1_00)
-      end
-
-      it 'casts zero integer values to a money instance' do
-        expect(product.zero_amount).to eq Money.new(0_00)
-      end
-
-      it 'preserves nil' do
-        expect(product.nil_amount).to eq nil
-      end
     end
   end
 end
