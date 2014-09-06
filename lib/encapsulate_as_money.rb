86c15f383278ea1932a7783a8b5dfcf9ef9141e6
Orien Madgwick <_@orien.io>
2014-09-06 22:30:24 +1000

    Support static currency definition

--- lib/encapsulate_as_money.rb
+++ lib/encapsulate_as_money.rb
@@ -3,30 +3,39 @@ require "encapsulate_as_money/version"
 require "money"
 
 module EncapsulateAsMoney
+  class CurrencyMismatchError < RuntimeError; end
+
   def encapsulate_as_money(*attributes)
     options = extract_options(attributes)
     attributes.each do |attribute|
-      encapsulate_attribute_as_money(attribute, options[:preserve_nil])
+      encapsulate_attribute_as_money(attribute, options[:currency], options[:preserve_nil])
     end
   end
 
 private
 
-  def encapsulate_attribute_as_money(attribute, preserve_nil = true)
+  def encapsulate_attribute_as_money(attribute, currency, preserve_nil = true)
+    currency = currency || Money.default_currency.id
     if preserve_nil
       define_method attribute do
-        Money.new(super()) if super()
+        Money.new(super(), currency) if super()
       end
 
       define_method "#{attribute}=" do |money|
+        if money && money.currency.id.upcase != currency.upcase
+          raise CurrencyMismatchError.new("Must be a quantity of #{currency.upcase}, received #{money.currency.id.upcase}")
+        end
         super(money && money.fractional)
       end
     else
       define_method attribute do
-        Money.new(super() || 0)
+        Money.new(super() || 0, currency)
       end
 
       define_method "#{attribute}=" do |money|
+        if money && money.currency.id.upcase != currency.upcase
+          raise CurrencyMismatchError.new("Must be a quantity of #{currency.upcase}, received #{money.currency.id.upcase}")
+        end
         num = (money && money.fractional) || 0
         super(num)
       end
--- spec/encapsulate_as_money_spec.rb
+++ spec/encapsulate_as_money_spec.rb
@@ -100,4 +100,49 @@ describe EncapsulateAsMoney do
       end
     end
   end
+
+  describe "encapsulating the attribute as money, defining currency AUD" do
+    When(:model_class) {
+      Class.new(model_base_class_with_attr) {
+        encapsulate_as_money :attribute, :currency => :aud
+      }
+    }
+
+    describe "reading" do
+      Given!(:init_attr_value) { model_instance.instance_variable_set :@attribute, initial_attr_value }
+
+      context "initial value is nil" do
+        Given(:initial_attr_value) { nil }
+        Then { model_instance.attribute == Money.new(0, :aud) }
+      end
+
+      context "initial value is 0" do
+        Given(:initial_attr_value) { 0 }
+        Then { model_instance.attribute == Money.new(0, :aud) }
+      end
+
+      context "initial value is 1" do
+        Given(:initial_attr_value) { 1 }
+        Then { model_instance.attribute == Money.new(1, :aud) }
+      end
+    end
+
+    describe "writing" do
+
+      context "a value of NZD $1" do
+        When(:result) { model_instance.attribute = Money.new(1_00, :nzd) }
+        Then { expect(result).to have_failed(EncapsulateAsMoney::CurrencyMismatchError, /Must be a quantity of AUD, received NZD/) }
+      end
+
+      context "a value of AUD $1" do
+        When { model_instance.attribute = Money.new(1_00, :aud) }
+        Then { model_instance.instance_variable_get(:@attribute) == 1_00 }
+      end
+
+      context "a value of nil" do
+        When { model_instance.attribute = nil }
+        Then { model_instance.instance_variable_get(:@attribute) == 0 }
+      end
+    end
+  end
 end
