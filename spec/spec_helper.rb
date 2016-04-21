fe6ee882f36929d0ec3d336782875cd2d51ce1c4
Zubin Henner <zubin@users.noreply.github.com>
2016-04-21 15:37:56 +1000

    Remove obsolete require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
 require "encapsulate_as_money"
 require "rspec/given"
-Dir[File.expand_path("../support/**/*.rb", __FILE__)].each { |f| require f }
 
 RSpec.configure do |config|
   config.color = true
